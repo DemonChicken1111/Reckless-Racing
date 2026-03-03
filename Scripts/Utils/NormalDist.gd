873de1f9dd7d3da5bb0d1ed561c396fb11227664
DemonChicken1111 <Eyeofakimoto@gmail.com>
2026-03-03 16:37:39 -0500

    Fixed NormalDist.gd location

--- Scripts/Utils/NormalDist.gd
+++ Scripts/Utils/NormalDist.gd
@@ -1,25 +1,18 @@
 class_name NormalDist
 extends Node
 
-# Generates RNG seed
 static func seed_gen():
 	var rng = RandomNumberGenerator.new()
-	
-	# Create random seed
-	rng.randomize()
-	
-	# Set custom seed
-	#rng.seed = 420
+	rng.seed = 420
 
 static func stat_normal_gen(base_stat: int) -> int:
 
 	var mean = 0
 	var variance = 1
 	
-	# Create normal distribution value, cannot be negative
 	var normal_dist = abs(randfn(mean, variance))
-	# Create the stat value (rounding up), clamp values to 10 and 100
-	var new_stat = clamp(ceili(base_stat * normal_dist), 10, 100)
-
+	var new_stat = ceili(base_stat * normal_dist)
+	
+	print(new_stat)
 	return new_stat
 	
